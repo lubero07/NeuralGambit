cc860c01ad387245d97259d2afd3ef313915ef68
Nivin <25b1813@iitb.ac.in>
2026-05-23 09:51:52 +0530

    sample_code

--- Week1/Warmup/Greedy_or_not.py
+++ Week1/Warmup/Greedy_or_not.py
@@ -1,35 +1,21 @@
-# Write your code here
+def best_score(nums, left, right):
+    if left > right:
+        return 0
+    total = sum(nums[left:right+1])
+    pick_left = nums[left] + total - nums[left] - best_score(nums, left+1, right)
+    pick_right = nums[right] + total - nums[right] - best_score(nums, left, right-1)
+    return max(pick_left, pick_right)
+
 n = int(input())
-nums = []
-for i in range(n):
-    x = int(input())
-    nums.append(x)
+nums = list(map(int, input().split()))
 
-score1 = 0
-score2 = 0
-turn = 1  # Player 1 starts
-
-while len(nums) > 0:
-    if turn == 1:
-        if nums[0] >= nums[-1]:
-            score1 += nums[0]
-            nums.pop(0)
-        else:
-            score1 += nums[-1]
-            nums.pop()
-        turn = 2
-    else:
-        if nums[0] >= nums[-1]:
-            score2 += nums[0]
-            nums.pop(0)
-        else:
-            score2 += nums[-1]
-            nums.pop()
-        turn = 1  
+total = sum(nums)
+score1 = best_score(nums, 0, n-1)
+score2 = total - score1
 
 if score1 > score2:
-    print("Player 1")
+    print("Player 1 wins")
 elif score2 > score1:
-    print("Player 2")
+    print("Player 2 wins")
 else:
-    print("tie")
+    print("Its a draw")
